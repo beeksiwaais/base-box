3fda2c4fa84661f8760ba3507b1caa8dd2b6a7e1
Beeksi Waais <skk@beeksiwaais.fr>
2016-01-29 15:37:27 +0100

    Fix Predictable Network Interface Names

--- config/packer/vagrant/scripts/ubuntu/networking.sh
+++ config/packer/vagrant/scripts/ubuntu/networking.sh
@@ -9,3 +9,18 @@ rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
 
 # Adding a 2 sec delay to the interface up, to make the dhclient happy
 echo "pre-up sleep 2" >>/etc/network/interfaces;
+
+
+# Restore networking see: https://gist.github.com/brbsix/b70413dec907906ef659
+# Disable Predictable Network Interface Names
+find /etc/default -name grub -exec sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0\"/g" {} \;
+update-grub
+
+# Reconfigure network intefaces
+echo "auto eth0" >>/etc/network/interfaces;
+echo "iface eth0 inet dhcp" >>/etc/network/interfaces;
+
+echo "auto eth1" >>/etc/network/interfaces;
+echo "iface eth1 inet dhcp" >>/etc/network/interfaces;
+
+
