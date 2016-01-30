b86317a221bd54b9bbef775278d093cdc6663784
Beeksi Waais <skk@beeksiwaais.fr>
2016-01-30 14:39:16 +0100

    Enable VMWare & Qemu for Atlas

--- config/packer/vagrant/scripts/common/vmtools.sh
+++ config/packer/vagrant/scripts/common/vmtools.sh
@@ -17,18 +17,6 @@ virtualbox-iso|virtualbox-ovf)
     rm -f $HOME_DIR/*.iso;
     ;;
 
-vmware-iso|vmware-vmx)
-    mkdir -p /tmp/vmfusion;
-    mkdir -p /tmp/vmfusion-archive;
-    mount -o loop $HOME_DIR/linux.iso /tmp/vmfusion;
-    tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive;
-    /tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --force-install;
-    umount /tmp/vmfusion;
-    rm -rf  /tmp/vmfusion;
-    rm -rf  /tmp/vmfusion-archive;
-    rm -f $HOME_DIR/*.iso;
-    ;;
-
 parallels-iso|parallels-pvm)
     mkdir -p /tmp/parallels;
     mount -o loop $HOME_DIR/prl-tools-lin.iso /tmp/parallels;
